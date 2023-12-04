0329eac1e138f3553285bb877f5dba95d3aa1a16
carlomarcon <carlomarcon1998@gmail.com>
2023-12-04 16:13:24 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script>
+export default {};
+</script>
+
+<template>
+  <i class="fa-solid fa-house"></i>
+</template>
+
+<style lang="scss">
+@use "../src/style/general.scss";
+@import "@fortawesome/fontawesome-free/css/all.css";
+</style>
